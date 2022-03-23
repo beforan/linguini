15743bc447af05930c32e9e458627b967dd62c4c
Jon Couldridge <jcouldridge@outlook.com>
2022-03-23 21:35:21 +0000

    refactoring into a subcommand, ready for more

---
+++ app/commands/export.js
@@ -0,0 +1,60 @@
+import fs from "fs";
+import path from "path";
+import xl from "excel4node";
+import { flattenLanguage } from "./helpers/flattenLanguage.js";
+
+export const exportAction = (options) => {
+  // load language data based on options
+  const languages = {};
+  fs.readdirSync(options.source)
+    .filter((file) => {
+      // confirm it's a directory
+      const stats = fs.lstatSync(path.join(options.source, file));
+      if (!stats.isDirectory) return false;
+
+      // always include base
+      if (options.base === file) return true;
+
+      // no languages option means no filtering
+      if (!options.languages) return true;
+
+      // else exclude languages not in the languages option
+      if (!options.languages?.includes(file)) return false;
+
+      return true;
+    })
+    .forEach((file) => {
+      // Currently we only load "core.json"
+      languages[file] = require(path.join(options.source, file, "core.json"));
+    });
+
+  // build output spreadsheet
+  const wb = new xl.Workbook();
+
+  const baseLng = flattenLanguage(languages[options.base]);
+
+  Object.keys(languages)
+    .filter((l) => l !== options.base)
+    .forEach((l) => {
+      const lng = flattenLanguage(languages[l]);
+
+      const ws = wb.addWorksheet(lng.name ?? l);
+
+      // Header row
+      ws.cell(1, 1).string("Key");
+      ws.cell(1, 2).string("Base Text");
+      ws.cell(1, 3).string(`${lng.name} Text`);
+
+      // each base key row
+      let row = 2;
+      Object.keys(baseLng).forEach((k) => {
+        ws.cell(row, 1).string(k);
+        ws.cell(row, 2).string(baseLng[k]);
+        ws.cell(row, 3).string(lng[k] ?? "");
+        row++;
+      });
+    });
+
+  // Write the output xlsx
+  wb.write(options.output);
+};
--- app/main.js
+++ app/main.js
@@ -1,17 +1,17 @@
 import { program } from "commander";
 import { createRequire } from "module";
-import fs from "fs";
-import path from "path";
-import xl from "excel4node";
-import { flattenLanguage } from "./helpers/flattenLanguage.js";
+import { exportAction } from "./commands/export.js";
 
 export const require = createRequire(import.meta.url);
 const pkg = require("../package.json");
 
+program.name(pkg.name).description(pkg.description).version(pkg.version);
+
 program
-  .name(pkg.name)
-  .description(pkg.description)
-  .version(pkg.version)
+  .command("export")
+  .description(
+    "Export locale files to a spreadsheet suitable for sending to a localisation team"
+  )
   .requiredOption("-s, --source <path>", "path to the source locales directory")
   .option(
     "-o, --output <path>",
@@ -22,62 +22,7 @@ program
     "-l, --languages <languages...>",
     "translated languages to include in the output"
   )
-  .option("-b, --base <language>", "base language", "dev");
+  .option("-b, --base <language>", "base language", "dev")
+  .action(exportAction);
 
 program.parse();
-
-const options = program.opts();
-
-// load language data based on options
-const languages = {};
-fs.readdirSync(options.source)
-  .filter((file) => {
-    // confirm it's a directory
-    const stats = fs.lstatSync(path.join(options.source, file));
-    if (!stats.isDirectory) return false;
-
-    // always include base
-    if (options.base === file) return true;
-
-    // no languages option means no filtering
-    if (!options.languages) return true;
-
-    // else exclude languages not in the languages option
-    if (!options.languages?.includes(file)) return false;
-
-    return true;
-  })
-  .forEach((file) => {
-    // Currently we only load "core.json"
-    languages[file] = require(path.join(options.source, file, "core.json"));
-  });
-
-// build output spreadsheet
-const wb = new xl.Workbook();
-
-const baseLng = flattenLanguage(languages[options.base]);
-
-Object.keys(languages)
-  .filter((l) => l !== options.base)
-  .forEach((l) => {
-    const lng = flattenLanguage(languages[l]);
-
-    const ws = wb.addWorksheet(lng.name ?? l);
-
-    // Header row
-    ws.cell(1, 1).string("Key");
-    ws.cell(1, 2).string("Base Text");
-    ws.cell(1, 3).string(`${lng.name} Text`);
-
-    // each base key row
-    let row = 2;
-    Object.keys(baseLng).forEach((k) => {
-      ws.cell(row, 1).string(k);
-      ws.cell(row, 2).string(baseLng[k]);
-      ws.cell(row, 3).string(lng[k] ?? "");
-      row++;
-    });
-  });
-
-// Write the output xlsx
-wb.write(options.output);
